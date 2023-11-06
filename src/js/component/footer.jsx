9b595fd4ef52a7ec36647784f069eb351fe1efe5
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-06 19:50:58 +0000

    landing page react

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="page-footer font-small text-white bg-dark pt-4">
+			<div className="footer-copyright text-center py-3">
+				Copyright © Your Website 2023
+			</div>
+		</footer>
+	);
+};
+
+export { Footer };

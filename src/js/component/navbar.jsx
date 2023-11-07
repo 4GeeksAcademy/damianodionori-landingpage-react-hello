87bb7c69bfb78ffa9d8023cc97ee8cbf4bd76e99
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-07 17:04:11 +0000

    navbar items fixed

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,44 +1,49 @@
 import React from "react";
 
 const NavItem = () => {
-
   const textItems = ["Home", "About", "Services", "Contact"];
-
   return (
-    <ul className="navbar-nav ms-auto mb-2 mb-lg-0" >{textItems.map((item, i) => {
-      return (
-        <li className="nav-item" key={i}>
-          <a className= {i===0 ? "nav-linnk-active" : "nav-link" } aria-current="page" href="#">{item}</a>
-        </li>
-      )
-    })}</ul>
-  )
-
-}
+    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+      {textItems.map((item, i) => {
+        return (
+          <li className="nav-item" key={i}>
+            <a
+              className={i === 0 ? "nav-link active" : "nav-link"}
+              aria-current="page"
+              href="#"
+            >
+              {item}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Start Bootstrap</a>
-          <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarText"
-                aria-controls="navbarText"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div
-                className="collapse navbar-collapse"
-                id="navbarText">
-                <NavItem />
-              </div>
-            </div>
-          </nav>
+        <a className="navbar-brand" href="#">
+          Start Bootstrap
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <NavItem />
+        </div>
+      </div>
+    </nav>
   );
 };
 

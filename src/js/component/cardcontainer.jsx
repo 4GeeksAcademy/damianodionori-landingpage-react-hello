1f46dd29312af26b20cf146f2533d3266e9e1998
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-10 11:32:25 +0000

    navitem

--- src/js/component/cardcontainer.jsx
+++ src/js/component/cardcontainer.jsx
@@ -6,7 +6,7 @@ import Card from "./card";
     return (
       <div style={{ display: 'flex' }}>
         <Card title="Card title" content="Is this ok, Leo? Please, accept my project. Mark it has approved!" button="Find Out More!" />
-        <Card title="Card title" content="Thank you for your help, Javier!" button="Find Out More!"/>
+        <Card title="Card title" content="Lorem ipsum bla bla bla bla bla bla!" button="Find Out More!"/>
         <Card title="Card title" content="Cras eleifend, eros ac finibus ultricies, nibh orci molestie augue, eget tempus libero dui id tortor." button="Find Out More!" />
         <Card title="Card title" content="Fusce venenatis aliquam massa, a lobortis ex condimentum eget." button="Find Out More!"/>
       </div>

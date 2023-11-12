ffc769d7f0f586002fb9359527e47fb86994442e
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-11-12 20:22:39 +0000

    button margins

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import PropTypes from 'prop-types'
 
 const Card = (props) => {
-    return (
-      <div className="card" style={{ width: '100%', margin: '15px', padding: '15px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <img src="https://placehold.co/500x325" className="card-img-top" alt="..." />
-        <div className="card-body text-center">
+  return (
+    <div className="card" style={{ width: '100%', margin: '15px', padding: '15px', display: 'flex', flexDirection: 'column' }}>
+      <img src="https://placehold.co/500x325" className="card-img-top" alt="..." />
+      <div className="card-body text-center" style={{ flex: '1', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <div>
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.content}</p>
-          <a href="#" className="btn btn-primary">
+        </div>
+        <div className="text-center" style={{ marginTop: '20px'}}>
+          <a href="#" className="btn btn-primary my-button">
             {props.button}
           </a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
   Card.propTypes = {
     content: PropTypes.string,
--- src/js/component/cardcontainer.jsx
+++ src/js/component/cardcontainer.jsx
@@ -4,7 +4,7 @@ import Card from "./card";
 
   const CardContainer = () => {
     return (
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
         <Card title="Card title" content="Is this ok, Leo? Please, accept my project. Mark it has approved!" button="Find Out More!" />
         <Card title="Card title" content="Lorem ipsum bla bla bla bla bla bla!" button="Find Out More!"/>
         <Card title="Card title" content="Cras eleifend, eros ac finibus ultricies, nibh orci molestie augue, eget tempus libero dui id tortor." button="Find Out More!" />
